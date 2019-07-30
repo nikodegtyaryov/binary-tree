db650711b5cbbbdda6b6b3f3204666805e02f46e
nikodegtyaryov <nikodegtyaryov@gmail.com>
2019-07-31 00:00:07 +0100

    Add search method for binary tree

--- lib/binary_tree.rb
+++ lib/binary_tree.rb
@@ -13,6 +13,16 @@ class BinaryTree
     if node.value < value
       self.left = BinaryTree.new if left.nil?
       left.insert(node)
+    elsif node.value > value
+      self.right = BinaryTree.new if right.nil?
+      right.insert(node)
     end
+  end
+
+  def search(node_value)
+    return nil if value.nil?
+    return self if node_value == value
+    return left.search(node_value) if node_value < value
+    return right.search(node_value) if node_value > value
   end
 end
--- spec/binary_tree_spec.rb
+++ spec/binary_tree_spec.rb
@@ -26,7 +26,7 @@ describe BinaryTree do
     it 'can be on the left side if value is lesser than the parent value' do
       tree.insert(right_node)
 
-      expect(tree.left).to eq(nil)
+      expect(tree.left).to be_nil
 
       tree.insert(left_node)
 
@@ -36,11 +36,50 @@ describe BinaryTree do
     it 'can be on the right side if value is greater than the parent value' do
       tree.insert(left_node)
 
-      expect(tree.right).to eq(nil)
+      expect(tree.right).to be_nil
 
       tree.insert(right_node)
 
       expect(tree.left.value).to eq(left_node.value)
+    end
+  end
+
+  context 'when searching a node' do
+    let(:tree) do
+      node = described_class.new(5)
+
+      [8, 3, 4, 2, 9, 7].each { |x| node.insert(described_class.new(x)) }
+      node
+    end
+
+    it 'can show nothing if tree is not populated' do
+      empty_tree = described_class.new
+
+      expect(empty_tree.search(11)).to be_nil
+    end
+
+    it 'can show bottom left node' do
+      node = tree.search(2)
+
+      expect(node.left).to be_nil
+      expect(node.right).to be_nil
+      expect(node.value).to eq(2)
+    end
+
+    it 'can show bottom right node' do
+      node = tree.search(9)
+
+      expect(node.left).to be_nil
+      expect(node.right).to be_nil
+      expect(node.value).to eq(9)
+    end
+
+    it 'can show top node' do
+      node = tree.search(5)
+
+      expect(node.left).to_not be_nil
+      expect(node.right).to_not be_nil
+      expect(node.value).to eq(5)
     end
   end
 end
